2e794b9c42ced429b3e6209fe92b8a12989dc930
Ivan Schneider Lopes <91276974+ivan-schneider@users.noreply.github.com>
2022-02-06 22:27:06 -0300

    Update item.dart

--- lib/src/item.dart
+++ lib/src/item.dart
@@ -7,17 +7,46 @@ class Materiais {
 }
 
 List<Materiais> absl24 = [
-  Materiais(nome: 'Laço para captura de animais', quantidade: 2, done: false),
-  Materiais(nome: 'Rede para captura de animais', quantidade: 1, done: false),
-  Materiais(nome: 'Protetor de airbag', quantidade: 1, done: false),
-  Materiais(nome: 'Cilindros de 9L', quantidade: 5, done: false),
-  Materiais(nome: 'Máscaras Panorâmicas', quantidade: 4, done: false),
-  Materiais(nome: 'Suporte Dorsal', quantidade: 4, done: false),
-  Materiais(nome: 'Cones grandes', quantidade: 9, done: false),
-  Materiais(nome: 'Caixa de Ferramentas', quantidade: 1, done: false),
-  Materiais(nome: 'Par de Nadadeiras', quantidade: 1, done: false),
-  Materiais(nome: 'Rescue', quantidade: 1, done: false),
-  Materiais(nome: 'Malho pequeno', quantidade: 1, done: false),
-  Materiais(nome: 'Protetor de Corda', quantidade: 1, done: false),
-  Materiais(nome: 'Calço de borracha', quantidade: 1, done: false),
-];
+  Materiais(nome: 'Adaptador Rosca Fêmea de 2½"', quantidade: 1, done: false),
+  Materiais(nome: 'Chave Dupla de Mangueira', quantidade: 1, done: false),
+  Materiais(nome: 'Chave tipo "J" GERMANY 90-155', quantidade: 1, done: false),
+  Materiais(nome: 'Chave Tripla de Mangueira', quantidade: 1, done: false),
+  Materiais(nome: 'Divisor com duas saídas de 1½" - AWG', quantidade: 1, done: false),
+  Materiais(nome: 'Esguicho de 1½" - AWG', quantidade: 1, done: false),
+  Materiais(nome: 'Esguicho de 1½" Sistema CAFS - JSCORPE', quantidade: 1, done: false),
+  Materiais(nome: 'Esguicho de 1½" - MULTIFLOW LEADER', quantidade: 1, done: false),
+  Materiais(nome: 'Esguicho de 2½" - PROTEC', quantidade: 1, done: false),
+  Materiais(nome: 'Esguicho de 1½" - AKRON TURBOJET', quantidade: 2, done: false),
+  Materiais(nome: 'Redução STORZ de 2½" para 1½" - AWG', quantidade: 2, done: false)
+      
+],
+
+List<Materiais> compartimento2 = [],
+
+List<Materiais> compartimento3 = [
+  Materiais(nome: 'Esguicho de 1½" - AWG', quantidade: 1, done: false),
+  Materiais(nome: 'Mangueira de 1½"', quantidade: 2, done: false)
+  
+],
+
+List<Materiais> compartimento4 = [
+  Materiais(nome: 'Aparelho portátil de corte - WEBER', quantidade: 1, done: false),
+  Materiais(nome: 'Divisor com duas saídas de 2½"', quantidade: 1, done: false),
+  Materiais(nome: 'Luva de Hidrante', quantidade: 3, done: false),
+  Materiais(nome: 'Mangueira de 2½"', quantidade: 3, done: false),
+  Materiais(nome: 'Mangueira de 1½"', quantidade: 5, done: false)
+      
+],
+
+List<Materiais> compartimento5 = [
+  Materiais(nome: 'Ventilador à Combustão - LEADER MT 236', quantidade: 1, done: false),
+  Materiais(nome: 'Cone Grande', quantidade: 10, done: false)
+  
+],
+
+List<Materiais> compartimento6 = [
+  Materiais(nome: 'Escada de Assalto', quantidade: 1, done: false),
+  Materiais(nome: 'Escada Dobrável', quantidade: 1, done: false),
+  Materiais(nome: 'Croque Bombeiro', quantidade: 2, done: false)
+  
+],
